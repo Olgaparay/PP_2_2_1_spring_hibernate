f5c41240f0815d6e0c6b9f00c7a085b2e7d94679
Olgaparay <olgaparaeva@hotmail.com>
2024-06-03 02:14:44 +0300

    добавление getUserCarModelAndCarSeries реализация сервиса для управления объектами

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -25,5 +25,10 @@ public class UserServiceImp implements UserService {
    public List<User> listUsers() {
       return userDao.listUsers();
    }
+   @Transactional(readOnly = true)
+   @Override
+   public List<User> getUserCarModelAndCarSeries(String model, int series){
+      return userDao.getUserCarModelAndCarSeries(model, series);
+   }
 
 }
